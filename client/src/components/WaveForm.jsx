6ab9dee4bbe09beb2d3108cb78a21a6f74cabbe2
Jesse Hang <hangtimee@gmail.com>
2019-04-13 12:58:11 -0700

    [width] fixed width

--- client/src/components/WaveForm.jsx
+++ client/src/components/WaveForm.jsx
@@ -5,7 +5,7 @@ const WaveForm = (props) => {
     <svg 
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w2.org/1999/xlink"
-      width="62.4vw"
+      width="1200px"
       height="150"
       viewBox="23 0 1194 176"
       preserveAspectRatio="none"
