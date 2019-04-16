ced142f649d802c70add9504351844d322e4be82
Jesse Hang <hangtimee@gmail.com>
2019-04-15 21:54:53 -0700

    [cors] added cors

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -24,21 +24,21 @@ export default class App extends Component {
 
   addToPlayer(id) {
     axios
-    .get(`/api/play/${id}`)
+    .get(`http://54.165.200.109:3069/api/play/${id}`)
     .then(data => data)
     .catch(err => console.error(err))
   }
 
   addToQueue(id) {
     axios
-    .get(`/api/queue/${id}`)
+    .get(`http://54.165.200.109:3069/api/queue/${id}`)
     .then(data => data)
     .catch(err => console.error(err))
   }
 
   fetchSongs() {
     axios
-    .get('/api/beats')
+    .get('http://54.165.200.109:3069/api/beats')
     .then(data => this.setState({allSongs: data.data}, () => {this.randomSongs(); this.randomTrack()}))
     .catch(err => console.error(err));
   }
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const router = require('./router')
 const port = 3069;
@@ -7,6 +8,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
 app.use(express.static(path.join(__dirname, "../client/dist")));
 app.use('/api', router);
 
